30560ecc92fa540ab44cd27e487c3c8ad6fdf2e1
jimztech <ibezimjames20@gmail.com>
2025-08-02 02:04:40 +0200

    heading done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const openMenu = document.getElementById("open-menu");
 const navBar = document.getElementById("nav-links");
+const links = document.querySelectorAll("[data-link]");
 
 openMenu.addEventListener("click", () => {
     if(navBar.style.display === "flex") {
@@ -11,4 +12,16 @@ openMenu.addEventListener("click", () => {
         openMenu.src = "./starter-code/assets/shared/icon-close.svg";
         navBar.classList.add("mobile");
     }
-})
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        links.forEach(linkItem => {
+            linkItem.classList.remove("border-white");
+            linkItem.classList.add("border-transparent");
+        });
+
+        link.classList.remove("border-transparent");
+        link.classList.add("border-white");
+    })
+});
