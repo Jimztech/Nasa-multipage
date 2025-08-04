036870dc6be5b7f8bd27b904f4d37f4465113846
jimztech <ibezimjames20@gmail.com>
2025-08-04 05:30:04 +0200

    worked on the hash routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const openMenu = document.getElementById("open-menu");
 const navBar = document.getElementById("nav-links");
 const links = document.querySelectorAll("[data-link]");
+const pages = document.querySelectorAll(".content-page");
 
 openMenu.addEventListener("click", () => {
     if(navBar.style.display === "flex") {
@@ -14,8 +15,32 @@ openMenu.addEventListener("click", () => {
     }
 });
 
+//Function ot show the pages
+function showSection(route) {
+    const targetId = `${route}-page`;
+
+    pages.forEach(page => {
+        page.style.display = "none";
+    });
+
+    const target = document.getElementById(targetId);
+    if(target) {
+        target.style.display = "block";
+    }
+}
+
 links.forEach(link => {
-    link.addEventListener("click", () => {
+    link.addEventListener("click", (e) => {
+
+        e.preventDefault();
+        const route = link.dataset.link;
+
+        // Changing the url hash
+        history.pushState(null, "", `#${route}`);
+
+        //Showing the new section
+        showSection(route);
+
         links.forEach(linkItem => {
             linkItem.classList.remove("border-white");
             linkItem.classList.add("border-transparent");
@@ -24,4 +49,16 @@ links.forEach(link => {
         link.classList.remove("border-transparent");
         link.classList.add("border-white");
     })
+});
+
+// Show page on initial load
+window.addEventListener("DOMContentLoaded", () => {
+    const initialRoute = location.hash ? location.hash.substring(1) : "home";
+    showSection(initialRoute);
+});
+
+// Handle forward and backward browser navigation.
+window.addEventListener("hashchange", () => {
+    const route = location.hash.substring(1);
+    showSection(route);
 });
