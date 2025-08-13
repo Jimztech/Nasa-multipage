655714e7a214d51292936b96ee7aca7f987ccf69
jimztech <ibezimjames20@gmail.com>
2025-08-13 22:42:18 +0200

    testing deployments

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ const numSwitch = document.querySelectorAll("[data-change]");
 openMenu.addEventListener("click", () => {
     if(navBar.style.display === "flex") {
         navBar.style.display = "none";
-        openMenu.src = "./starter-code/assets/shared/icon-hamburger.svg";
+        openMenu.src = "./assets/shared/icon-hamburger.svg";
         navBar.classList.remove("mobile");
     } else {
         navBar.style.display = "flex";
-        openMenu.src = "./starter-code/assets/shared/icon-close.svg";
+        openMenu.src = "./assets/shared/icon-close.svg";
         navBar.classList.add("mobile");
     }
 });
